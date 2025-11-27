44ad7bf7d78a133d867d2c4303cf334017a27bff
Luan Morais <luan.antonio@estudantes.ifg.edu.br>
2025-11-27 14:47:17 -0300

    Atualização

---
+++ Programa_de_Registro_Escolar/src/model/Diario.java
@@ -0,0 +1,83 @@
+package model;
+import java.util.List;
+
+public class Diario {
+
+    private int id;
+    private boolean status;
+    private List<Aluno> aluno;
+    private List<Disciplina> disciplina;
+    private List<Periodo> periodo;
+    private List<Turma> turma;
+    private Nota nota;
+
+    public Diario() {
+    }
+
+    public Diario(int id, boolean status, List<Aluno> aluno, List<Disciplina> disciplina, List<Periodo> periodo,
+            List<Turma> turma, Nota nota) {
+        this.id = id;
+        this.status = status;
+        this.aluno = aluno;
+        this.disciplina = disciplina;
+        this.periodo = periodo;
+        this.turma = turma;
+        this.nota = nota;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public List<Aluno> getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(List<Aluno> aluno) {
+        this.aluno = aluno;
+    }
+
+    public List<Disciplina> getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(List<Disciplina> disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public List<Periodo> getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(List<Periodo> periodo) {
+        this.periodo = periodo;
+    }
+
+    public List<Turma> getTurma() {
+        return turma;
+    }
+
+    public void setTurma(List<Turma> turma) {
+        this.turma = turma;
+    }
+
+    public Nota getNota() {
+        return nota;
+    }
+
+    public void setNota(Nota nota) {
+        this.nota = nota;
+    }
+}
